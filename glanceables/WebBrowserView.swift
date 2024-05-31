af775626904d24fa6820b7d51c28b2a0edafc779
devin.r.liu@gmail.com <devin.r.liu@gmail.com>
2024-05-30 19:33:02 -0700

    Initialized webview grid for glanceables

---
+++ glanceables/WebBrowserView.swift
@@ -0,0 +1,18 @@
+//
+//  WebBrowserView.swift
+//  glanceables
+//
+//  Created by Devin Liu on 5/30/24.
+//
+
+import SwiftUI
+
+struct WebBrowserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebBrowserView()
+}
