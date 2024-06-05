7da4e5b0aed55f8c72c69a6169b893f84f8b0b25
devin.r.liu@gmail.com <devin.r.liu@gmail.com>
2024-06-04 17:31:20 -0700

    Extracted URL Modal to its own swift file

---
+++ URLModalView.swift
@@ -0,0 +1,18 @@
+//
+//  URLModalView.swift
+//  glanceables
+//
+//  Created by Devin Liu on 6/4/24.
+//
+
+import SwiftUI
+
+struct URLModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    URLModalView()
+}
---
+++ glanceables/UrlModalView.swift
@@ -0,0 +1,18 @@
+//
+//  UrlModalView.swift
+//  glanceables
+//
+//  Created by Devin Liu on 6/4/24.
+//
+
+import SwiftUI
+
+struct UrlModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UrlModalView()
+}
