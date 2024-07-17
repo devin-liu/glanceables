96a6ad4248d54f1b633fbbd8342b343d78213ccd
devin.r.liu@gmail.com <devin.r.liu@gmail.com>
2024-07-17 13:20:57 -0700

    Positioned the red x button to a negative offset of the top right webclip capture menu

--- WebPreviewCaptureMenuView.swift
+++ WebPreviewCaptureMenuView.swift
@@ -6,69 +6,77 @@ struct WebPreviewCaptureMenuView: View {
     @ObservedObject var captureMenuViewModel: WebPreviewCaptureMenuViewModel
     
     var body: some View {
-        VStack{
-            HStack {
+        ZStack {            
+            VStack{
                 AddURLFormView(viewModel: viewModel)
-                RedXButton(action: viewModel.resetModalState)
-            }
-            .cornerRadius(10)
-            .padding(10)
-            
-            HStack {
-                if !viewModel.isURLValid && !viewModel.urlString.isEmpty {
-                    Text("Invalid URL").foregroundColor(.red)
+                HStack {
+                    if !viewModel.isURLValid && !viewModel.urlString.isEmpty {
+                        Text("Invalid URL").foregroundColor(.red)
+                    }
+                    Button("Save") {
+                        viewModel.saveURL(with: viewModel.screenShot)
+                    }.frame(width: 80, height: 40)
+                    
                 }
-                Button("Save") {
-                    viewModel.saveURL(with: viewModel.screenShot)
-                }.frame(width: 80, height: 40)
                 
-            }
-            
-            if let screenshot = $viewModel.screenShot.wrappedValue, captureMenuViewModel.showPreview {
-                Image(uiImage: screenshot)
-                    .frame(width: 300, height: 300)
-                    .padding()
-            }
-            
-            if viewModel.isURLValid && !captureMenuViewModel.showPreview {
-                GeometryReader { geometry in
-                    ZStack {
-                        WebViewScreenshotCapture(viewModel: viewModel, captureMenuViewModel: captureMenuViewModel)
-                            .frame(maxHeight: .infinity)
-                            .frame(width: geometry.size.width)
-                            .gesture(
-                                DragGesture(minimumDistance: 0)
-                                    .onChanged { value in
-                                        captureMenuViewModel.startLocation = captureMenuViewModel.startLocation ?? value.location
-                                        captureMenuViewModel.endLocation = value.location
-                                        captureMenuViewModel.dragging = true
-                                        updateClipRect(endLocation: value.location, bounds: geometry.size)
+                if let screenshot = $viewModel.screenShot.wrappedValue, captureMenuViewModel.showPreview {
+                    Image(uiImage: screenshot)
+                        .frame(width: 300, height: 300)
+                        .padding()
+                }
+                
+                if viewModel.isURLValid && !captureMenuViewModel.showPreview {
+                    GeometryReader { geometry in
+                        ZStack {
+                            WebViewScreenshotCapture(viewModel: viewModel, captureMenuViewModel: captureMenuViewModel)
+                                .frame(maxHeight: .infinity)
+                                .frame(width: geometry.size.width)
+                                .gesture(
+                                    DragGesture(minimumDistance: 0)
+                                        .onChanged { value in
+                                            captureMenuViewModel.startLocation = captureMenuViewModel.startLocation ?? value.location
+                                            captureMenuViewModel.endLocation = value.location
+                                            captureMenuViewModel.dragging = true
+                                            updateClipRect(endLocation: value.location, bounds: geometry.size)
+                                        }
+                                        .onEnded { _ in
+                                            captureMenuViewModel.dragging = false
+                                            //                                        showPreview = true
+                                        }
+                                )
+                            if captureMenuViewModel.captureModeOn {
+                                if let clipRect = viewModel.currentClipRect {
+                                    if captureMenuViewModel.dragging {
+                                        Rectangle()
+                                            .stroke(style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round, dash: [10, 5]))
+                                            .path(in: clipRect)
+                                            .background(Color.black.opacity(0.1))
                                     }
-                                    .onEnded { _ in
-                                        captureMenuViewModel.dragging = false
-                                        //                                        showPreview = true
-                                    }
-                            )
-                        if captureMenuViewModel.captureModeOn {
-                            if let clipRect = viewModel.currentClipRect {
-                                if captureMenuViewModel.dragging {
                                     Rectangle()
-                                        .stroke(style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round, dash: [10, 5]))
+                                        .stroke(style: StrokeStyle(lineWidth: 1, lineCap: .round, lineJoin: .round, dash: [10, 5]))
                                         .path(in: clipRect)
-                                        .background(Color.black.opacity(0.1))
                                 }
-                                Rectangle()
-                                    .stroke(style: StrokeStyle(lineWidth: 1, lineCap: .round, lineJoin: .round, dash: [10, 5]))
-                                    .path(in: clipRect)
                             }
                         }
                     }
                 }
+                Spacer()
             }
-            Spacer()
+            .background(Color.white) // Set the overall background of the VStack to white
+            .cornerRadius(10)
+            // Red 'X' Button Positioned Absolutely at the Top-Right
+            VStack {
+                HStack {
+                    Spacer() // Push everything to the right
+                    RedXButton(action: viewModel.resetModalState)
+                        .padding(.top, -20) // Negative top padding to move the button outside
+                        .padding(.trailing, -20) // Negative top padding to move the button outside
+                }
+                Spacer() // Push everything to the top
+            }
         }
-        .background(Color.white) // Set the overall background of the VStack to white
-        .cornerRadius(10)
+        
+        
     }
     
     private func updateClipRect(endLocation: CGPoint, bounds: CGSize) {
--- glanceables/ContentView.swift
+++ glanceables/ContentView.swift
@@ -95,3 +95,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
