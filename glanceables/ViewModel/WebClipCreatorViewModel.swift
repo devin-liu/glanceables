2ee727835c3a725372237717f2ab7647ea50d40c
devin.r.liu@gmail.com <devin.r.liu@gmail.com>
2024-08-13 15:36:14 -0700

    Removed comments from WebClipCreatorViewModel

--- glanceables/ViewModel/WebClipCreatorViewModel.swift
+++ glanceables/ViewModel/WebClipCreatorViewModel.swift
@@ -15,15 +15,7 @@ class WebClipCreatorViewModel: ObservableObject {
     private var urlStringCancellable: AnyCancellable? // To hold the subscription
     private var cancellables: Set<AnyCancellable> = []
     
-    @Published var validURLs: [URL] = []  // Now storing an     array of URLs
-    @Published var selectedValidURLIndex: Int? = nil {
-        didSet {
-            print("selectedValidURLIndex", validURLs)
-//            if let index = selectedValidURLIndex, validURLs.indices.contains(index) {
-//                urlString = validURLs[index].absoluteString
-//            }
-        }
-    }
+    @Published var validURLs: [URL] = []
     @Published var currentClipRect: CGRect?
     @Published var isURLValid = true
     @Published var showValidationError = false
@@ -37,19 +29,9 @@ class WebClipCreatorViewModel: ObservableObject {
     private var webClip: PendingWebClip = PendingWebClip()
     private var repository = WebClipUserDefaultsRepository.shared
     
-    init() {
-        //          setupURLStringSubscription()
-    }
+    init() {}
     
-    //      private func setupURLStringSubscription() {
-    //          $urlString
-    //              .removeDuplicates()
-    //              .debounce(for: 0.3, scheduler: DispatchQueue.main)
-    //              .sink(receiveValue: validateURL)
-    //              .store(in: &cancellables)
-    //      }
-    
-    var validURL: URL? {        
+    var validURL: URL? {
         return validURLs.last
     }
     
@@ -79,16 +61,13 @@ class WebClipCreatorViewModel: ObservableObject {
         capturedElements = newElements
     }
     
-    
     func saveScreenShot(_ newScreenShot: UIImage, toClip:WebClip?=nil) -> String? {
         screenShot = newScreenShot
         if let newScreenshotPath = ScreenshotUtils.saveScreenshotToLocalDirectory(screenshot: newScreenShot) {
             screenshotPath = newScreenshotPath
         }
-        
         return nil
     }
-    
     
     func saveOriginalSize(newOriginalSize: CGSize) {
         originalSize = newOriginalSize
@@ -97,73 +76,26 @@ class WebClipCreatorViewModel: ObservableObject {
     private func validateURL(_ urlString: String) {
         debouncer.cancel()
         print("validateURL ", urlString)
-        //          // Implement URL validation logic here, update isURLValid accordingly
-        //          isURLValid = URL(string: urlString) != nil
-        //          showValidationError = !isURLValid && !urlString.isEmpty
-        //
         let (isValid, url) = URLUtilities.validateURL(from: urlString)
         print("run validateURL ", urlString, url)
         isURLValid = isValid
-        if isValid, let url = url {
-            validURLs.append(url)
-            selectedValidURLIndex = 0
-        }
-        //                if let url = url {
-        //                    if validURLs.isEmpty {
-        //                        validURLs.append(url)
-        //                        selectedValidURLIndex = 0 // Initialize the index with the first URL
-        //                    } else {
-        //                        updateOrAddValidURL(url)
-        //                    }
-        //                }
-    }
-    
-    //    func validateURL() {
-    //        let (isValid, url) = URLUtilities.validateURL(from: urlString)
-    //        print("run validateURL ", urlString, url)
-    //        isURLValid = isValid
-    //        if let url = url {
-    //            if validURLs.isEmpty {
-    //                validURLs.append(url)
-    //                selectedValidURLIndex = 0 // Initialize the index with the first URL
-    //            } else {
-    //                updateOrAddValidURL(url)
-    //            }
-    //        }
-    //    }
-    //
-    func updateOrAddValidURL(_ newURL: URL) {
-        if let selectedIndex = selectedValidURLIndex,
-           let currentURL = validURL,
-           let newDomain = URLUtilities.extractDomain(from: newURL.absoluteString),
-           let currentDomain = URLUtilities.extractDomain(from: currentURL.absoluteString),
-           newDomain == currentDomain {
-            validURLs[selectedIndex] = newURL // Replace the URL at the current index if domains match
-        } else {
-            // There is no selected index or domains are not provided; skip domain checking
-            print("No selected index or domain provided; adding URL")
-            validURLs.append(newURL)
-            selectedValidURLIndex = validURLs.count - 1 // Update the index to the new URL if not set
-        }
     }
 }
-
-
 
 class Debouncer {
     private var workItem: DispatchWorkItem?
     private let interval: TimeInterval
-
+    
     init(seconds: TimeInterval) {
         self.interval = seconds
     }
-
+    
     func debounce(action: @escaping () -> Void) {
         workItem?.cancel()
         workItem = DispatchWorkItem(block: action)
         DispatchQueue.main.asyncAfter(deadline: .now() + interval, execute: workItem!)
     }
-
+    
     // Ensure this method is properly defined
     func cancel() {
         workItem?.cancel()
