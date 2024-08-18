bd3fe2b5730286dab6fb828b51f45bedf1e71b7e
devin.r.liu@gmail.com <devin.r.liu@gmail.com>
2024-08-17 17:34:48 -0700

    Moved snapshot refreshers to a separate managed UI array than the displayed grid

--- glanceables/View/WebClipGridView.swift
+++ glanceables/View/WebClipGridView.swift
@@ -6,14 +6,14 @@ struct WebClipGridView: View {
     
     var body: some View {
         WebClipGridItems(webClips: webClipManager.getClips())
-        .animation(.easeInOut, value: webClipManager.getClips())
+            .animation(.easeInOut, value: webClipManager.getClips())
     }
 }
 
 struct WebClipGridItems: View {
     @Environment(WebClipManagerViewModel.self) private var webClipManager
     var webClips: [WebClip]
-        
+    
     let columns = [GridItem(.adaptive(minimum: 300))]
     
     var body: some View {
@@ -28,15 +28,7 @@ struct WebClipGridItems: View {
                     
                     WebGridSingleSnapshotView(item: item)
                     
-                    WebViewSnapshotRefresher(webClipId: item.id)
-                                 .frame(width: item.originalSize?.width, height: 600)
-                                 .edgesIgnoringSafeArea(.all)
-                                 .opacity(0)  // Make the ScrollView invisible
-                                 .frame(width: 0, height: 0)  // Make the ScrollView occupy no space
-                                 .onDisappear {
-                                     print("WebViewSnapshotRefresher onDisappear")
-                                 }
-                       
+                    
                 } .contextMenu {
                     NavigationLink(destination: WebClipEditorView(webClipId: item.id)) {
                         Text("Edit")
@@ -45,6 +37,19 @@ struct WebClipGridItems: View {
                         webClipManager.deleteItemById(item.id)
                     }
                 }
+            }
+        }
+        
+        LazyVGrid(columns: columns) {
+            ForEach(webClips, id: \.self) { item in
+                WebViewSnapshotRefresher(webClipId: item.id)
+                    .frame(width: item.originalSize?.width, height: 600)
+                    .edgesIgnoringSafeArea(.all)
+                    .opacity(0)  // Make the ScrollView invisible
+                    .frame(width: 0, height: 0)  // Make the ScrollView occupy no space
+                    .onDisappear {
+                        print("WebViewSnapshotRefresher onDisappear")
+                    }
             }
         }
     }
